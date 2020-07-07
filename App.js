74d33693e20cac24a29ab94c70b3fc7dc29c85f1
Leila Skinner <beileiwang@gmail.com>
2020-07-07 15:09:15 -0500

    learned about safeareaview and text

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
-  console.log("App executed");
+  const handlePress = () => {
+    console.log("Text Clicked");
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
+    // view is a container component for react native
+    // SafeAreaView adds padding to the top ex. avoid the top black notch, etc.
+    <SafeAreaView style={styles.container}>
+      {/* always wrap text in Text component */}
+      <Text numberOfLines={2} onPress={handlePress}>
+        Hello React Native - A really really long text. Now I wanna make this
+        line even longer and see what happens!
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "yellow",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });

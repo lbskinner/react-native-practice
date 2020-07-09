1f8f147cf095cfc585cb93a26910c2bb718202b5
Leila Skinner <beileiwang@gmail.com>
2020-07-09 13:45:44 -0500

    welcome screen with log and button placeholders created

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Welcome from "./Welcome";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Welcome></Welcome>
+      <WelcomeScreen />
     </View>
   );
 }

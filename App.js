9d8539426bfee0732d9878527387f99cc9b9a25c
Leila Skinner <beileiwang@gmail.com>
2020-07-13 14:35:07 -0500

    created button component

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <WelcomeScreen />
+      {/* <WelcomeScreen /> */}
       {/* <ViewImageScreen /> */}
+      <AppButton title="Login" onPress={() => console.log("Button Clicked")} />
     </View>
   );
 }
@@ -16,6 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
+    // flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { ImageBackground, StyleSheet, Image, Text, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  Button,
+} from "react-native";
 // the library below is part of expo package
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 // can import AppText this way when an index.js files has been created in the AppText file
 import AppText from "../components/AppText";
 // import AppText from "../components/AppText/AppText";
+
+import colors from "../config/colors";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 function WelcomeScreen() {
   return (
@@ -17,9 +27,13 @@ function WelcomeScreen() {
         <Image source={require("../assets/logo-red.png")} style={styles.logo} />
         <AppText>Sell What You Don't Need</AppText>
       </View>
-      <MaterialCommunityIcons name="email" size={100} color="dodgerblue" />
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      {/* <MaterialCommunityIcons name="email" size={100} color="dodgerblue" /> */}
+      <View style={styles.loginButton}>
+        <Button title="Login" color="#fff" />
+      </View>
+      <View style={styles.registerButton}>
+        <Button title="Register" color="#fff" />
+      </View>
     </ImageBackground>
   );
 }
@@ -47,7 +61,7 @@ const styles = StyleSheet.create({
   registerButton: {
     width: "100%",
     height: 70,
-    backgroundColor: "#4ecdc4",
+    backgroundColor: colors.secondary,
   },
 });
 

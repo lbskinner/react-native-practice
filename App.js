1373189989058c19376ddcbfcfff26339a911125
Leila Skinner <beileiwang@gmail.com>
2020-07-13 15:24:50 -0500

    created card component

--- App.js
+++ App.js
@@ -4,14 +4,28 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
-
+import Card from "./app/components/Card";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <WelcomeScreen />
-      {/* <ViewImageScreen /> */}
-      {/* <AppButton title="Login" onPress={() => console.log("Button Clicked")} /> */}
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/jacket.jpg")}
+      />
     </View>
+
+    // <View style={styles.container}>
+    //   <WelcomeScreen />
+    //   {/* <ViewImageScreen /> */}
+    //   {/* <AppButton title="Login" onPress={() => console.log("Button Clicked")} /> */}
+    // </View>
   );
 }
 
--- app/components/AppText/AppText.js
+++ app/components/AppText/AppText.js
@@ -1,12 +1,18 @@
 // custom text component for different platform
 import React from "react";
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 
-import styles from "./styles";
+// import styles from "./styles";
 
 // destructure the children props so don't need to use props.children
-function AppText({ children }) {
-  return <Text style={styles.text}>{children}</Text>;
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
 }
 
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+});
 export default AppText;

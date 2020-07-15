cf3337cdd2a6c0158f2a0d7cda18e0771b8dedfd
Leila Skinner <beileiwang@gmail.com>
2020-07-15 17:48:20 -0500

    created icon component

--- App.js
+++ App.js
@@ -7,16 +7,23 @@ import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+
 export default function App() {
   return (
     // <ListingDetailsScreen />
 
-    <View style={styles.container}>
-      {/* <WelcomeScreen /> */}
-      {/* <ViewImageScreen /> */}
-      {/* <AppButton title="Login" onPress={() => console.log("Button Clicked")} /> */}
-      <MessagesScreen />
-    </View>
+    // <View style={styles.container}>
+    //   <WelcomeScreen />
+    //   <ViewImageScreen />
+    //   <AppButton title="Login" onPress={() => console.log("Button Clicked")} />
+    //   <MessagesScreen />
+    // </View>
+
+    <Screen>
+      <Icon name="email" size={100} />
+    </Screen>
   );
 }
 

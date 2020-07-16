5d467286f1630ce18ec59607bfb5d6bf130e887c
Leila Skinner <beileiwang@gmail.com>
2020-07-16 14:58:48 -0500

    learn and test text input

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
@@ -14,7 +14,25 @@ import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+  return (
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+        secureTextEntry // same as secureTextEntry={true}
+        // clearButtonMode onlys works on IOS
+        clearButtonMode="always"
+        keyboardType="numeric"
+        maxLength={5}
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({

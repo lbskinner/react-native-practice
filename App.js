0abe707e7d97558e5b6b2981a63057403c820381
Leila Skinner <beileiwang@gmail.com>
2020-07-22 16:26:11 -0500

    created basic image input list component

--- App.js
+++ App.js
@@ -5,42 +5,24 @@ import { StyleSheet, Button, Image } from "react-native";
 
 import Screen from "./app/components/Screen";
 import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
-  const requestPermission = async () => {
-    // permissions API can pass multiple permission types
-    // this method also returns a promise
-    // const result = await Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION)
-    // method above is the identical to the method below (requestCameraRollPermissionsAsync())
-    // the request method internally uses permissions API
-    const { granted } = await ImagePicker.requestCameraRollPermissionsAsync();
-    if (!granted) alert("You need to enable permission to access the library.");
+  const [imageUris, setImageUris] = useState([]);
+
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
   };
 
-  // function in the useEffect hook gets executed every time the component gets rendered
-  // useEffect has an optional second perimeter - an array of dependencies
-  // anytime any of these variables (the variables passed in the array) changes, the function will get re-executed
-  // when pass an empty array, the function only gets executed once since there are no dependencies
-  // useEffect can not accept a function that returns a promise
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri);
-    } catch (error) {
-      console.log("Error reading an image: ", error);
-    }
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
   };
-
   return (
     <Screen>
-      <ImageInput
-        imageUri={imageUri}
-        onChangeImage={(uri) => setImageUri(uri)}
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
       />
     </Screen>
   );

e7e6b736b583109b133f95e1c9ebe133e4ab90ae
Leila Skinner <beileiwang@gmail.com>
2020-07-22 16:34:40 -0500

    added scroll view to image input list component

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,16 +1,35 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  // useRef to call methods on a component
+  // useRef returns a general purpose object
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <View key={uri} style={styles.image}>
-          <ImageInput imageUri={uri} onChangeImage={() => onRemoveImage(uri)} />
+    <View>
+      {/* by default scrollView grows to take all available spaces
+          when this component is used in other components/screens
+          it will push down other fields to take all available spaces 
+          add View to solve this, because the size of View is determined by the size of its content */}
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View key={uri} style={styles.image}>
+              <ImageInput
+                imageUri={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
         </View>
-      ))}
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+      </ScrollView>
     </View>
   );
 }

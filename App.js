74a80149c5344250015f1e9656334f098b338aa3
Leila Skinner <beileiwang@gmail.com>
2020-07-28 13:30:17 -0500

    learn about navigation between screens

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
-import { StyleSheet, Button, Image } from "react-native";
+import { StyleSheet, Button, Image, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
 import ImageInput from "./app/components/ImageInput";
@@ -9,8 +11,37 @@ import ImageInputList from "./app/components/ImageInputList";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+);
+
+// createStackNavigator gives an object
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  // the StackNavigator components knows how to navigate between the different screens
+  <Stack.Navigator initialRouteName="Tweets">
+    {/* first screen is the default screen showing or use initialRouteName */}
+    {/* name is used to define the routes and shows as the title of the bar  */}
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    <Stack.Screen name="Tweets" component={Tweets} />
+  </Stack.Navigator>
+);
+
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      {/* render StackNavigator component instead of rendering the specific screens */}
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({

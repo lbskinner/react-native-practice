f6d4832608ca6dbd3bc94a7579d1c1039f356c46
Leila Skinner <beileiwang@gmail.com>
2020-08-11 16:16:46 -0500

    added offline notice

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
-import { StyleSheet, Button, Image, Text } from "react-native";
+import { StyleSheet, Button, Image, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Screen from "./app/components/Screen";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
@@ -21,6 +19,7 @@ import AccountScreen from "./app/screens/AccountScreen";
 import AppNavigator from "./app/navigation/AppNavigator";
 import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 import AsyncStorage from "@react-native-community/async-storage";
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
   // fetch() only fetch connection info once
@@ -32,7 +31,7 @@ export default function App() {
   // unsubscribe();
 
   // use hooks in functional component
-  const netInfo = useNetInfo();
+  // const netInfo = useNetInfo();
 
   const demo = async () => {
     try {
@@ -52,11 +51,14 @@ export default function App() {
     // netInfo.isInternetReachable ? <View></View> : <View></View>
     // another common approach
     // <Button disabled={!netInfo.isInternetReachable} title="Button" />
-    <NavigationContainer theme={navigationTheme}>
-      {/* render StackNavigator component instead of rendering the specific screens */}
-      {/* <AuthNavigator /> */}
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        {/* render StackNavigator component instead of rendering the specific screens */}
+        {/* <AuthNavigator /> */}
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
 
---
+++ app/components/OfflineNotice.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+import { useNetInfo } from "@react-native-community/netinfo";
+
+import Text from "./Text";
+import colors from "../config/colors";
+
+function OfflineNotice(props) {
+  const netInfo = useNetInfo();
+  console.log(netInfo);
+
+  if (netInfo.type !== "unknown" && netInfo.isInternetReachable === false)
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>No Internet Connect</Text>
+      </View>
+    );
+
+  return null;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.primary,
+    height: 50,
+    position: "absolute",
+    top: Constants.statusBarHeight,
+    width: "100%",
+    // brings the component to the top
+    zIndex: 1,
+  },
+  text: {
+    color: colors.white,
+  },
+});
+
+export default OfflineNotice;

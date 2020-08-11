4facf3e2c6d06e7173138ead220ead3283b8d9ad
Leila Skinner <beileiwang@gmail.com>
2020-08-11 15:50:11 -0500

    added listing image to listing details screen

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Image } from "react-native-expo-image-cache";
 
 import Text from "../components/Text";
 import colors from "../config/colors";
@@ -10,7 +11,13 @@ function ListingDetailsScreen({ route }) {
   console.log(listing);
   return (
     <View>
-      <Image style={styles.image} source={listing.image} />
+      {/* replace Image from react-native to react-native-expo-image-cache */}
+      <Image
+        style={styles.image}
+        preview={{ uri: listing.images[0].thumbnailUrl }}
+        tint="light"
+        uri={listing.images[0].url}
+      />
       <View style={styles.detailsContainer}>
         <Text style={styles.title}>{listing.title}</Text>
         <Text style={styles.price}>${listing.price}</Text>
